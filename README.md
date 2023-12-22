cffbf28ac8aabd2b9f71bf002ce986d35e9cba59
Mario Mori <mario_mori@hotmail.com>
2023-12-22 17:09:30 -0300

    Se encuentra finalizada la pagina de dolar blue 2023

---
+++ README.md
@@ -0,0 +1,23 @@
+# Instrucciones
+
+Para correr la aplicación ejecutar lo siguiente para Windows
+
+```
+python -m venv venv
+.\venv\Scripts\activate
+pip install -r requirements.txt
+python -m flask --app .\src\app.py run
+```
+
+Para correr la aplicación ejecutar lo siguiente para Linux o Mac
+
+```
+python -m venv venv
+./venv/bin/activate
+pip install -r requirements.txt
+python -m flask --app ./src/app.py run
+```
+
+# URLs
+
+La pagina de datos sobre el dolar la vamos a encontrar en localhost:5000/data
---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import os
+import json
+import re
+
+BASE_DIR = os.getcwd()
+DATABASE_DIR = os.path.join(BASE_DIR, "data")
+
+app = Flask(__name__)
+
+@app.route("/data")
+def data():
+    database = os.path.join(DATABASE_DIR, "evolution.json")
+    data = []
+    with open(database, "r") as data_file:
+        data_j = json.loads(data_file.read())[::-1]
+        for d in data_j:
+            if re.search(r'2023', d['date']) and d['source'] == "Blue":
+                data.append(d)
+    return render_template("dataPage.html", title="Historial del dolar", data=data)
