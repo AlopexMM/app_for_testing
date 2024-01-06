c425a5fbf8ccb3e3b37ee02bc8e071122f0cf3a3
mario <mariomori00@gmail.com>
2024-01-06 12:09:40 -0300

    Added Mercado de Cervezas

--- src/app.py
+++ src/app.py
@@ -14,6 +14,10 @@ def index():
         {
             "title": "Cotización del dolar blue 2023",
             "url":url_for('data'),
+        },
+        {
+            "title": "Mercado de cervezas",
+            "url": url_for("beer_shop"),
         }
     ]
     return render_template("index.html", title="Paginas disponibles", data=data)
@@ -28,3 +32,15 @@ def data():
             if re.search(r'2023', d['date']) and d['source'] == "Blue":
                 data.append(d)
     return render_template("dataPage.html", title="Historial del dolar", data=data)
+
+@app.route("/beer-shop")
+def beer_shop():
+    database = os.path.join(DATABASE_DIR, "shop.json")
+    data = []
+    with open(database, "r") as data_file:
+        data_j = json.loads(data_file.read())
+        for d in data_j:
+            img = d["image"]
+            d["image"] = os.path.join("/static/media", img)
+            data.append(d)
+    return render_template("beerShop.html", title="Mercado de Cervezas", data=data)
