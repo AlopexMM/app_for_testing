69bcd778d3f7187353dc8b923bf38c11e8ce6cf7
Mario Mori <mario_mori@hotmail.com>
2024-04-15 12:44:51 -0300

    Se termino el setup de playwright

---
+++ playwright.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig, devices } from 'playwright/test';
+
+export default defineConfig({
+    // Look for test files in the "tests" directory, relative to this configuration file.
+    testDir: 'tests/playwright/',
+
+    // Run all tests in parallel.
+    fullyParallel: true,
+
+    // Reporter to use
+    reporter: 'html',
+
+    use: {
+        // Base URL to use in actions like `await page.goto('/')`.
+        baseURL: 'http://127.0.0.1:3001',
+
+        // Collect trace when retrying the failed test.
+        trace: 'on-first-retry',
+    },
+    // Configure projects for major browsers.
+    projects: [
+    {
+        name: 'chromium',
+        use: { ...devices['Desktop Chrome'] },
+    },
+    ],
+    // Run your local dev server before starting the tests.
+    // webServer: {
+    //     command: 'npm run start',
+    //     url: 'http://127.0.0.1:3000',
+    //     reuseExistingServer: !process.env.CI,
+    // },
+});
